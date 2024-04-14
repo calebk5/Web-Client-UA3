8fd0b60a07807f44bceeb94d8f67ca06f9f416a9
calebk5 <kastnercaleb@gmail.com>
2024-04-14 16:16:00 -0400

    Changé certains noms de fichier, arrangé beaucoup de petites choses

---
+++ Script/Script_Animation.js
@@ -0,0 +1,36 @@
+function cakechange() {
+    var element = document.getElementById('pastry');
+    element.classList.remove("muffin");
+    element.classList.add("cake");
+
+    document.getElementById('totoko')
+     .src="../Images/Images_animation/Totoko.Animation.Chefsigh.png";
+
+     setTimeout(function (){
+          document.getElementById('totoko')
+          .src="../Images/Images_animation/Totoko.Animation.Chefbase.png";
+     }, 3000);
+}
+
+
+function muffinchange() {
+    var element = document.getElementById('pastry');
+    element.classList.remove("cake");
+    element.classList.add("muffin");
+
+    document.getElementById('totoko')
+     .src="../Images/Images_animation/Totoko.Animation.Chefchin.png";
+
+     setTimeout(function (){
+        document.getElementById('totoko')
+        .src="../Images/Images_animation/Totoko.Animation.Chefbase.png";
+    }, 3000);
+}
+
+
+
+
+document.getElementById("buttoncake").addEventListener("click", function(){
+    var animationTarget = document.querySelector("pastry");
+    animationTarget.classList.add("ovendough");
+  });
