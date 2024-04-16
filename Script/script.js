5be833aaab6eb3808ab043b2dd856ad1acef98c8
calebk5 <kastnercaleb@gmail.com>
2024-04-15 22:15:22 -0400

    Ajouté footer et pleins de bugs

--- Script/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var hamburger = document.querySelector('.hamburger');
-    var navUL = document.querySelector('nav ul');
-
-    hamburger.addEventListener('click', function() {
-        navUL.classList.toggle('nav-active');
-    });
-});
